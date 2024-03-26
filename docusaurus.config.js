fcfd191151bbb88b8bcb85cc0552d0c4d7da5cc3
jason.aw <jason.ahw@masverse.com.my>
2024-03-26 11:02:47 +0800

    add favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Masverse Enterprise Docs',
   tagline: 'Innovation meets success',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/masfavicon.png',
 
   // Set the production url of your site here
   url: 'https://docs-maschain.vercel.app/',
