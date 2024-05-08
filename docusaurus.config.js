b5b558c19c3e9dc301480392aee9a6143f50ebaf
chan <Chan.VS@masverse.com.my>
2024-05-08 14:54:22 +0800

    update subpath

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/masfavicon.png',
 
   // Set the production url of your site here
-  url: 'https://masverse.github.io/masverse-portal-docs/',
+  url: 'https://masverse.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
