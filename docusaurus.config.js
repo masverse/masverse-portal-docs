82245db823dcad6dbf88c59d26e280109cf26e1a
chan <Chan.VS@masverse.com.my>
2024-05-08 15:31:51 +0800

    update gitaction and config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,9 +27,6 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-
-  cname.vercel-dns.com
-
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
